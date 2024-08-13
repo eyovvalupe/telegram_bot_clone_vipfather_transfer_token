dff325d89171426460a158bb861cd2580ac5916a
eyovvalupe <rylantwcn705172@hotmail.com>
2024-08-13 17:28:25 +0800

    using /generate to create new tron wallet

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api")
-const { telegramApiToken } = require("./config")
+const { telegramApiToken, tronFullHost } = require("./config")
+const TronWeb = require("tronweb")
 
 const bot = new TelegramBot(telegramApiToken, { polling: true })
 
@@ -11,6 +12,24 @@ bot.onText(/\/start/, msg => {
 bot.onText(/\/help/, msg => {
     const chatId = msg.chat.id
     bot.sendMessage(chatId, "Available commands:\n/generate - Generate a new Tron address\n/transfer - Transfer TRX to another address\n/balance - Check your TRX balance")
+})
+
+bot.onText(/\/generate/, msg => {
+    const chatId = msg.chat.id
+
+    const tronWeb = new TronWeb({
+        fullHost: tronFullHost,
+    })
+
+    const account = tronWeb.createAccount()
+    account
+        .then(newAccount => {
+            bot.sendMessage(chatId, `New Tron Address Generated:\nAddress: ${newAccount.address.base58}\nPrivate Key: ${newAccount.privateKey}`)
+        })
+        .catch(err => {
+            console.error("Error generating Tron address:", err)
+            bot.sendMessage(chatId, "Failed to generate a new Tron address. Please check the log.")
+        })
 })
 
 console.log("Bot is running...")
--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ require("dotenv").config()
 
 module.exports = {
     telegramApiToken: process.env.TELEGRAM_API_TOKEN,
+    tronFullHost: process.env.TRON_FULL_HOST,
 }
